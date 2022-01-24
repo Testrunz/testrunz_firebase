3aa858bb236aeb5529e49893d7397668152c83eb
shahanapa <shahanapa712@gmail.com>
2022-01-24 18:34:32 +0530

    script

--- server/routes/scripts/ElectricElectronic.py
+++ server/routes/scripts/ElectricElectronic.py
@@ -23,3 +23,43 @@ class EE:
         E=(E1+E2+E3+E4+E5)/5
         R=(R1+R2+R3+R4+R5)/5
         print(json.dumps({"answer":[{"result":"Thus the efficiency and regulation of a transformer is predetermined by conducting open circuit test and short circuit test and the equivalent circuit is drawn.","Efficiency":str(E),"Regulation":str(R)}]}))
+    def INDUCTION(self):
+        argument = self.arg[0:]
+        #Torque
+        T1 = 9.81*((float(argument[3])-float(argument[4]))+2)*0.0986
+        T2 = 9.81*((float(argument[10])-float(argument[11]))+2)*0.0986
+        T3 = 9.81*((float(argument[17])-float(argument[18]))+2)*0.0986
+        T4 = 9.81*((float(argument[24])-float(argument[25]))+2)*0.0986
+        T5 = 9.81*((float(argument[31])-float(argument[32]))+2)*0.0986
+                      
+        #input
+        IP1 = float(argument[5]) * float(argument[6])
+        IP2 = float(argument[12]) * float(argument[13])
+        IP3 = float(argument[19]) * float(argument[20])
+        IP4 = float(argument[26]) * float(argument[27])
+        IP5 = float(argument[33]) * float(argument[34])
+
+            #output
+        OP1 = (2*math.pi*float(argument[7])*T1)/60
+        OP2 = (2*math.pi*float(argument[14])*T2)/60
+        OP3 = (2*math.pi*float(argument[21])*T3)/60
+        OP4 = (2*math.pi*float(argument[28])*T4)/60
+        OP5 = (2*math.pi*float(argument[35])*T5)/60
+
+        #Eff
+        Eff1 = (OP1/IP1)*100
+        Eff2 = (OP2/IP2)*100
+        Eff3 = (OP3/IP3)*100
+        Eff4 = (OP4/IP4)*100
+        Eff5 = (OP5/IP5)*100
+        Eff = (Eff1 + Eff2 + Eff3 + Eff4 + Eff5)/5
+
+        Ns = 1500
+        Reg1 = ((Ns - float(argument[7]))/Ns)*100
+        Reg2 = ((Ns - float(argument[14]))/Ns)*100
+        Reg3 = ((Ns - float(argument[21]))/Ns)*100
+        Reg4 = ((Ns - float(argument[29]))/Ns)*100
+        Reg5 = ((Ns - float(argument[36]))/Ns)*100
+        Reg = (Reg1 + Reg2 + Reg3 + Reg4 + Reg5)/5
+        print(json.dumps({"Mean":[{"The efficiency of single phase induction motor is" : str(Eff)}] , "Mean":[{"The Regularity is" : str(Reg)}]}))
+        
